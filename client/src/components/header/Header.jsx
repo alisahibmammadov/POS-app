17cf14e700b751f665f937bf8a19bb0d83460d6a
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-07 21:31:38 +0400

    Header complated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,89 @@
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Input, Badge, Avatar } from "antd";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="border-b mb-6">
+      <header className="py-4 px-6 flex justify-between items-center gap-10">
+        <div className="logo">
+          <a href="/">
+            <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
+          </a>
+        </div>
+        <div className="header-search flex-1 flex justify-center items-center">
+          <Input
+            size="large"
+            placeholder="Ürün ara..."
+            prefix={<SearchOutlined />}
+            className="rounded-full  max-w-[800px]"
+          />
+        </div>
+        <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <HomeOutlined className="text-xl md:text-2xl " />
+            <span className="text-[10px] md:text-xs ">Ana Sayfa</span>
+          </a>
+          <Badge offset={[0,6]} count={5} className="hidden md:flex ">
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+              <ShoppingCartOutlined className="text-xl md:text-2xl " />
+              <span className="text-[10px] md:text-xs ">Sepet</span>
+          </a>
+            </Badge>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <CopyOutlined className="text-xl md:text-2xl " />
+            <span className="text-[10px] md:text-xs ">Faturalar</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <UserOutlined className="text-xl md:text-2xl " />
+            <span className="text-[10px] md:text-xs ">Musteriler</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <BarChartOutlined className="text-xl md:text-2xl " />
+            <span className="text-[10px] md:text-xs ">Statistikler</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <LogoutOutlined className="text-xl md:text-2xl " />
+            <span className="text-[10px] md:text-xs ">Çıkış</span>
+          </a>
+        </div>
+        <Badge offset={[0,6]} count={5} className="md:hidden flex">
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+              <ShoppingCartOutlined className="text-2xl " />
+              <span className="text-[10px] md:text-xs ">Sepet</span>
+          </a>
+            </Badge>
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
